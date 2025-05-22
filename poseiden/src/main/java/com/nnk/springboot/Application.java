1df8e2d0951f4a620f5392f74ee1ae805023c365
AIR01 <erwann.bruneaupro@gmail.com>
2025-05-22 20:28:54 +0200

    profile dev

--- poseiden/src/main/java/com/nnk/springboot/Application.java
+++ poseiden/src/main/java/com/nnk/springboot/Application.java
@@ -8,6 +8,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);
-		application.setAdditionalProfiles("prod");// profile
+		application.setAdditionalProfiles("dev");// profile
 	}
 }
