4627470bd2e9bb0ab9421c050e0d4e806b36dcb7
AIR01 <erwann.bruneaupro@gmail.com>
2025-05-22 20:19:22 +0200

    add profile

--- poseiden/src/main/java/com/nnk/springboot/Application.java
+++ poseiden/src/main/java/com/nnk/springboot/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		System.setProperty("spring.profiles.active", "prod");
-		SpringApplication.run(Application.class, args);
+		SpringApplication application = new SpringApplication(Application.class);
+		application.setAdditionalProfiles("dev");// profile
 	}
 }
